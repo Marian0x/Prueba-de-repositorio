cb9a92651a30e7bbccb6497f6341b70da46ec686
Marian0x <139175413+Marian0x@users.noreply.github.com>
2026-05-02 19:45:42 -0300

    Creacion de archivo prueba.py

---
+++ prueba.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+class MainApp(tk.Tk):
+    def __init__(self):
+      super().__init__()
+      self.geometry("200x300)
+      self.title("Primera ventanita")
+
+if __name__=="__main__":
+    app=MainApp()
+    app.mainloop()
