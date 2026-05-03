b78b6a6d646c098dffedcc66159b8d67919d1077
Marian0x <139175413+Marian0x@users.noreply.github.com>
2026-05-02 22:05:38 -0300

    Update prueba.py
    Se corrige tamaño de ventana

--- prueba.py
+++ prueba.py
@@ -3,7 +3,7 @@ import tkinter as tk
 class MainApp(tk.Tk):
     def __init__(self):
       super().__init__()
-      self.geometry("200x300)
+      self.geometry("200x300")
       self.title("Primera ventanita")
 
 if __name__=="__main__":
